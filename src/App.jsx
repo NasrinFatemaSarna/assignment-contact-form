05b426e40bf717188c35cc5329b9f20464e2342f
NasrinFatemaSarna <162792355+NasrinFatemaSarna@users.noreply.github.com>
2026-01-19 23:57:35 +0600

    Fix GitHub Pages routing with basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AddContact from "./pages/AddContact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/assignment-contact-form">
       <Routes>
         <Route path="/" element={<AllContacts />} />
         <Route path="/add" element={<AddContact />} />
