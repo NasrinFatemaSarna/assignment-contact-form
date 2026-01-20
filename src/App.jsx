e29ba17cc8059d5490338e4fe674e5d87e5490fe
NasrinFatemaSarna <162792355+NasrinFatemaSarna@users.noreply.github.com>
2026-01-20 16:44:34 +0600

    fix: router basename duplication and name display

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import AddContactPage from "./pages/AddContactPage.jsx";
+import HomePage from "./pages/HomePage";
+import AddContactPage from "./pages/AddContactPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/assignment-contact-form" element={<HomePage />} />
-      <Route path="/assignment-contact-form/add" element={<AddContactPage />} />
-
-      <Route path="/" element={<Navigate to="/assignment-contact-form" replace />} />
-      <Route path="/add" element={<Navigate to="/assignment-contact-form/add" replace />} />
-
-      <Route path="*" element={<Navigate to="/assignment-contact-form" replace />} />
+     
+      <Route path="/" element={<HomePage />} />
+      <Route path="/add" element={<AddContactPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
+
