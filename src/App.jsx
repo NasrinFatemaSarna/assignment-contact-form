2fffa24e2c34c50fc806de827af4dc230cc4cdc7
NasrinFatemaSarna <162792355+NasrinFatemaSarna@users.noreply.github.com>
2026-01-20 16:51:13 +0600

    fix: router basename duplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AddContactPage from "./pages/AddContactPage";
+import HomePage from "./pages/HomePage.jsx";
+import AddContactPage from "./pages/AddContactPage.jsx";
 
 export default function App() {
   return (
     <Routes>
-     
       <Route path="/" element={<HomePage />} />
       <Route path="/add" element={<AddContactPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ContactProvider } from "./context/ContactContext";
+import App from "./App.jsx";
 import "./index.css";
+import { ContactProvider } from "./context/ContactContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
